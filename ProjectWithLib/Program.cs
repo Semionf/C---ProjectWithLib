0ee7ef9bf231174d0c42bfab81689c49da971790
semionf <sfurlender@gmail.com>
2022-11-24 12:00:59 +0200

    .

---
+++ ProjectWithLib/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RandomNumber;
+namespace ProjectWithLib
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 numbers = new Class1();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(numbers.randomNum() + numbers.awesomeSentence());
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RandomNumber/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomNumber
+{
+    public class Class1
+    {
+        public Random rnd = new Random(DateTime.Now.Millisecond);
+        public int randomNum()
+        {
+            int randomNum = rnd.Next();
+            return randomNum;
+        }
+
+        public string awesomeSentence()
+        {
+            return "C# is the best";
+        }
+    }
+}
